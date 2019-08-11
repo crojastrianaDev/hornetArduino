b6f70447781fd36915f0fed2978e20b11cc3dc12
crojastriana3 <cristianrojas@unimayor.edu.co>
2019-08-11 15:39:52 -0500

    Archivos ignorados

---
+++ HolaMundo.ino
@@ -0,0 +1,18 @@
+
+/*
+ * Hola Mundo con Arduino, realizado por Alejandro Rojas de la tienda de porductos HORNET.
+ */
+void setup()
+{
+  Serial.begin(9600);// Abrimos el puerto serial con una velocidad de 9600 baudios
+  
+
+}
+
+void loop()
+{
+  
+  Serial.println("HOLA MUNDO DESDE HORNET");// Imprimimos nuestro Hola Mundo en consola, la consola se encuentra en herramientas-Monitor serie.
+  // No olvide ingresar a herramientas y configurar su placa, procesador y el puerto al cual esta conectado su placa.
+
+}
