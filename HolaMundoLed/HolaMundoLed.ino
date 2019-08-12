2bdfbe239caaf8ac6a73a234ffaaec6fa4255798
crojastriana3 <cristianrojas@unimayor.edu.co>
2019-08-11 20:00:54 -0500

    Update HolaMundoLed.ino

--- HolaMundoLed/HolaMundoLed.ino
+++ HolaMundoLed/HolaMundoLed.ino
@@ -1,17 +1,18 @@
 /*
  * Hola mundo, parpadeo de un led, Realizado por: Alejandro Rojas de HORNET.
  */
-int led = 13;
+int led = 13;// Creamos la variable para el led en el pin 13.
 void setup()
 {
-  pinMode(led,OUTPUT);
+  pinMode(led,OUTPUT);//Indicamos el modo de pin, en este caso de salida.
 
 }
 
 void loop() 
 {
- digitalWrite(led,HIGH);
- delay(1000);
- digitalWrite(led,LOW);
- delay(500);
+ digitalWrite(led,HIGH);//Endendemos el led
+ delay(1000);//Damos un segundo de espera.
+ digitalWrite(led,LOW);//Apagamos el led
+ delay(500);//Damos medio segundo de espera.
+ //Se repite el ciclo.
 }
